e75fdf282042714d36caa4bda3482554f151e4b4
gorra87 <ibrahimagora.diouf1@unchk.edu.sn>
2026-03-21 11:39:26 +0000

    mon deuxieme commit

---
+++ src/composants/accueil.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+function Text(){
+  return( 
+        <p className="text-success fw-900 fs-4 uppercase">bonjur</p>
+  )
+}
+
+export default function Accueil(){
+    return (
+    <div>
+    <Text/>
+    <Text/>
+    <Text/>
+    </div>
+    )
+}
